22ca5581102b0caaca1f53ca323a50ff150b1838
Michal4566 <240779@edu.p.lodz.pl>
2022-01-18 19:32:58 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_GIT_01.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_GIT_01.dir/main.cpp.obj"
+  "OU_GIT_01.exe"
+  "OU_GIT_01.exe.manifest"
+  "OU_GIT_01.pdb"
+  "libOU_GIT_01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_GIT_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
